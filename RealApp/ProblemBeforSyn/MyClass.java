cda89731db24bdb355b7eddd04e4c67221abb44a
EyadBahaa1 <96442182+EyadBahaa1@users.noreply.github.com>
2022-12-18 16:08:01 +0200

    Add files via upload

---
+++ RealApp/ProblemBeforSyn/MyClass.java
@@ -0,0 +1,44 @@
+ public class MyClass {
+    public static void main(String args[])
+    {
+           RoomBooking obj=new RoomBooking();
+           
+           Thread thread1=new Thread(obj,"Client1");
+           Thread thread2=new Thread(obj,"Client2");
+           
+           thread1.start();
+           thread2.start();
+           
+    }
+ 
+}
+
+
+
+
+ class RoomBooking implements Runnable{
+    int RoomAvailable=1;
+    
+    public void run(){
+           
+           System.out.println("Waiting to book Room for : "+Thread.currentThread().getName());
+           if(RoomAvailable>0){
+                  System.out.println("Booking Room for : "+Thread.currentThread().getName());
+                  
+                  //Let's say system takes some time in booking Room (here we have taken 1 second time)
+                  try{
+                        Thread.sleep(1000); 
+                  }catch(Exception e){}
+                    
+                  RoomAvailable--;
+                  System.out.println("Room BOOKED for : "+ Thread.currentThread().getName());
+                  System.out.println("currently RoomAvailable = "+RoomAvailable);
+           }
+           else{
+                  System.out.println("Room NOT BOOKED for : "+ Thread.currentThread().getName());
+           }
+           
+           
+    }
+    
+}
